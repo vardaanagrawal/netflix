e43da72705c172e46584084bdd6b6625b34f1f2d
Vardaan <vardaanagrawal517@gmail.com>
2023-06-03 08:05:02 +0530

    removed profiles

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Profile from "../components/profile/Profile";
+// import Profile from "../components/profile/Profile";
 import Home from "../components/home/Home";
 import Movies from "../components/home/movies/Movies";
 
@@ -8,8 +8,8 @@ export default function Routess() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<Profile />} />
-        <Route exact path="/browse" element={<Home />}>
+        {/* <Route exact path="/" element={<Profile />} /> */}
+        <Route exact path="/" element={<Home />}>
           <Route exact path="" element={<Movies />}></Route>
         </Route>
       </Routes>
